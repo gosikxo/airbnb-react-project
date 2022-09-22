362db18bbac310804f2330b7213a16652ae26b7d
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-09-22 15:28:59 +0300

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react"
-
-/*
-Challenge: Build the Navbar component.
-Check the Figma file for the design specifics.
-*/
+import { Navbar } from "./components/Navbar"
+import './style.css'
 
 export default function App() {
     return (
-        <h1>App component</h1>
+        <div className="App">
+          <Navbar />
+        </div>
     )
 }
