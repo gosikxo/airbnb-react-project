e1a297f753e928fdb288ed07f5e35d2c314591ad
Gosia Brozek <gosikbrozek1997@gmail.com>
2022-09-23 14:48:24 +0300

    Add hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const Hero = () => {
+  return (
+    <div className='hero'>
+            <img className='heroImg' src='../photo.jpg'/> 
+            <h1 className='heroTitle'>Online Experiences</h1>
+            <h3 className='heroText'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</h3>
+    </div>
+  )
+}
